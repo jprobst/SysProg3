5430fd2c9c616b23aa3b078e70eef384e05ea1b6
jprobst <jaquelineprobst@gmail.com>
2016-04-14 17:57:15 -0400

    1
    Creado formulario de Acceso

---
+++ Form1.Designer.vb
@@ -0,0 +1,124 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmAcceso
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmAcceso))
+        Me.lbUsuario = New System.Windows.Forms.Label()
+        Me.txtUsuario = New System.Windows.Forms.TextBox()
+        Me.txtContrasena = New System.Windows.Forms.TextBox()
+        Me.lbContrasena = New System.Windows.Forms.Label()
+        Me.btnAceptar = New System.Windows.Forms.Button()
+        Me.btnCancelar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lbUsuario
+        '
+        Me.lbUsuario.Image = Global.SysProg3.My.Resources.Resources.group
+        Me.lbUsuario.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.lbUsuario.Location = New System.Drawing.Point(52, 44)
+        Me.lbUsuario.Name = "lbUsuario"
+        Me.lbUsuario.Size = New System.Drawing.Size(61, 20)
+        Me.lbUsuario.TabIndex = 0
+        Me.lbUsuario.Text = "      Usuario"
+        Me.lbUsuario.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'txtUsuario
+        '
+        Me.txtUsuario.Location = New System.Drawing.Point(127, 44)
+        Me.txtUsuario.Name = "txtUsuario"
+        Me.txtUsuario.Size = New System.Drawing.Size(226, 20)
+        Me.txtUsuario.TabIndex = 1
+        '
+        'txtContrasena
+        '
+        Me.txtContrasena.Location = New System.Drawing.Point(127, 83)
+        Me.txtContrasena.Name = "txtContrasena"
+        Me.txtContrasena.Size = New System.Drawing.Size(226, 20)
+        Me.txtContrasena.TabIndex = 3
+        '
+        'lbContrasena
+        '
+        Me.lbContrasena.Image = Global.SysProg3.My.Resources.Resources.key
+        Me.lbContrasena.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.lbContrasena.Location = New System.Drawing.Point(38, 83)
+        Me.lbContrasena.Name = "lbContrasena"
+        Me.lbContrasena.Size = New System.Drawing.Size(83, 20)
+        Me.lbContrasena.TabIndex = 2
+        Me.lbContrasena.Text = "Contraseña"
+        Me.lbContrasena.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'btnAceptar
+        '
+        Me.btnAceptar.Image = Global.SysProg3.My.Resources.Resources.accept
+        Me.btnAceptar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnAceptar.Location = New System.Drawing.Point(197, 123)
+        Me.btnAceptar.Name = "btnAceptar"
+        Me.btnAceptar.Size = New System.Drawing.Size(75, 35)
+        Me.btnAceptar.TabIndex = 4
+        Me.btnAceptar.Text = "Aceptar"
+        Me.btnAceptar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
+        Me.btnAceptar.UseVisualStyleBackColor = True
+        '
+        'btnCancelar
+        '
+        Me.btnCancelar.Image = Global.SysProg3.My.Resources.Resources.cancel
+        Me.btnCancelar.Location = New System.Drawing.Point(278, 123)
+        Me.btnCancelar.Name = "btnCancelar"
+        Me.btnCancelar.Size = New System.Drawing.Size(75, 35)
+        Me.btnCancelar.TabIndex = 5
+        Me.btnCancelar.Text = "Cancelar"
+        Me.btnCancelar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
+        Me.btnCancelar.UseVisualStyleBackColor = True
+        '
+        'frmAcceso
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.ClientSize = New System.Drawing.Size(405, 176)
+        Me.Controls.Add(Me.btnCancelar)
+        Me.Controls.Add(Me.btnAceptar)
+        Me.Controls.Add(Me.txtContrasena)
+        Me.Controls.Add(Me.lbContrasena)
+        Me.Controls.Add(Me.txtUsuario)
+        Me.Controls.Add(Me.lbUsuario)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmAcceso"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Acceso al Sistema"
+        Me.TransparencyKey = System.Drawing.Color.White
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lbUsuario As System.Windows.Forms.Label
+    Friend WithEvents txtUsuario As System.Windows.Forms.TextBox
+    Friend WithEvents txtContrasena As System.Windows.Forms.TextBox
+    Friend WithEvents lbContrasena As System.Windows.Forms.Label
+    Friend WithEvents btnAceptar As System.Windows.Forms.Button
+    Friend WithEvents btnCancelar As System.Windows.Forms.Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,3 @@
+﻿Public Class frmAcceso
+
+End Class
